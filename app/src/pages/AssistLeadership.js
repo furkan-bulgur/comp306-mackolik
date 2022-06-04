b714412a838797579cc783160ebb395bc9798bd2
bozercavdar <83980769+bozercavdar@users.noreply.github.com>
2022-06-04 14:24:12 +0300

    Replace Link with Navigate

--- app/src/pages/AssistLeadership.js
+++ app/src/pages/AssistLeadership.js
@@ -1,56 +1,55 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 function AssistLeadership(props) {
-    if (props.assisters) {
-      return (
-        <div className="league_table">
-          <table>
-            <thead>
-              <tr>
-                <th>Rank</th>
-                <th>Team</th>
-                <th>Player Name</th>
-                <th>Played Match</th>
-                <th>Played Minutes</th>
-                <th>Goals</th>
-                <th>Assists</th>
-                <th>Assists Per Match</th>
+  const navigate = useNavigate();
+  if (props.assisters) {
+    return (
+      <div className="league_table">
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Team</th>
+              <th>Player Name</th>
+              <th>Played Match</th>
+              <th>Played Minutes</th>
+              <th>Goals</th>
+              <th>Assists</th>
+              <th>Assists Per Match</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.assisters.map((assister, index) => (
+              <tr key={assister.pid}>
+                <td>{index + 1}</td>
+                {Object.values(assister)
+                  .slice(2)
+                  .map((val, inner) => {
+                    if (inner === 0) {
+                      return (
+                        <td
+                          className="navigatable"
+                          onClick={() => {
+                            navigate(`/team/squad/${assister.tid}`);
+                          }}
+                        >
+                          {val}
+                        </td>
+                      );
+                    } else {
+                      return <td>{val}</td>;
+                    }
+                  })}
               </tr>
-            </thead>
-            <tbody>
-              {props.assisters.map((assister, index) => (
-                <tr key={assister.pid}>
-                  <td>{index + 1}</td>
-                  {Object.values(assister)
-                    .slice(2)
-                    .map((val, inner) => {
-                      if (inner === 0) {
-                        return (
-                          <td>
-                            <Link
-                              to={{
-                                pathname: `/team/squad/${assister.tid}`,
-                              }}
-                            >
-                              {val}
-                            </Link>
-                          </td>
-                        );
-                      } else {
-                        return <td>{val}</td>;
-                      }
-                    })}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    return <div></div>;
   }
+}
 
 export default AssistLeadership;
--- app/src/pages/GoalLeadership.js
+++ app/src/pages/GoalLeadership.js
@@ -1,55 +1,55 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function GoalLeadership(props) {
-    if (props.scorers) {
-      return (
-        <div className="league_table">
-          <table>
-            <thead>
-              <tr>
-                <th>Rank</th>
-                <th>Team</th>
-                <th>Player Name</th>
-                <th>Played Match</th>
-                <th>Played Minutes</th>
-                <th>Goals</th>
-                <th>Assists</th>
-                <th>Goal Per Match</th>
+  const navigate = useNavigate();
+  if (props.scorers) {
+    return (
+      <div className="league_table">
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Team</th>
+              <th>Player Name</th>
+              <th>Played Match</th>
+              <th>Played Minutes</th>
+              <th>Goals</th>
+              <th>Assists</th>
+              <th>Goal Per Match</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.scorers.map((scorer, index) => (
+              <tr key={scorer.pid}>
+                <td>{index + 1}</td>
+                {Object.values(scorer)
+                  .slice(2)
+                  .map((val, inner) => {
+                    if (inner === 0) {
+                      return (
+                        <td
+                          className="navigatable"
+                          onClick={() => {
+                            navigate(`/team/squad/${scorer.tid}`);
+                          }}
+                        >
+                          {val}
+                        </td>
+                      );
+                    } else {
+                      return <td>{val}</td>;
+                    }
+                  })}
               </tr>
-            </thead>
-            <tbody>
-              {props.scorers.map((scorer, index) => (
-                <tr key={scorer.pid}>
-                  <td>{index + 1}</td>
-                  {Object.values(scorer)
-                    .slice(2)
-                    .map((val, inner) => {
-                      if (inner === 0) {
-                        return (
-                          <td>
-                            <Link
-                              to={{
-                                pathname: `/team/squad/${scorer.tid}`,
-                              }}
-                            >
-                              {val}
-                            </Link>
-                          </td>
-                        );
-                      } else {
-                        return <td>{val}</td>;
-                      }
-                    })}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  } else {
+    return <div></div>;
   }
+}
 
 export default GoalLeadership;
